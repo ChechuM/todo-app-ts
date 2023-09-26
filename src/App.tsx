bd46957663483504645563d023c5d6bfacd2aff4
ChechuM <ceciliammoroni@gmail.com>
2023-09-27 09:14:46 +1000

    added Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import { Todos } from './components/Todos'
-import { type FilterValue, type TodoId, type Todo as TodoType, type TodoTitle } from './types'
+import { type FilterValue, type TodoId, type Todo as TodoType, type TodoTitle, type ListOfToDos } from './types'
 import { TODO_FILTERS } from './const'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
@@ -23,55 +23,107 @@ const mockToDos = [
   }
 ]
 
+type reducerAction = {
+  type: 'addTodo'
+  payload: string
+} | {
+  type: 'completeTodo'
+  payload: string
+} | {
+  type: 'removeTodo'
+  payload: string
+} | {
+  type: 'removeCompleted'
+}
+
+// REDUCER FUNCTION
+const todoReducer = (state: ListOfToDos, action: reducerAction): ListOfToDos => {
+  switch (action.type) {
+    case 'addTodo':{
+      const newTodo = {
+        id: crypto.randomUUID(),
+        title: action.payload,
+        completed: false
+      }
+      return [...state, newTodo] }
+
+    case 'completeTodo':{
+      const CompleteId = action.payload
+      const newTodos = state.map(todo => {
+        if (todo.id === CompleteId) {
+          return {
+            ...todo,
+            // copia todo el resto del objeto y para el caso del completed -> lo invierte
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return newTodos }
+
+    case 'removeTodo':{
+      const RemoveId = action.payload
+      const lessTodos = state.filter(todo => todo.id !== RemoveId)
+      return lessTodos
+    }
+
+    case 'removeCompleted' :{
+      const pendingTodos = state.filter(todo => !todo.completed)
+      return pendingTodos
+    }
+
+    default:
+      return state
+  }
+}
+
 const App = (): JSX.Element => {
-  const [todos, setTodos] = useState(mockToDos)
+  const INITIAL_STATE = mockToDos
+  // REDUCER DECLARATION
+  const [state, dispatch] = useReducer(todoReducer, INITIAL_STATE)
+
+  // const [todos, setTodos] = useState(mockToDos) -> lo hemos reemplazado por un Reducer
   // hay que avisarle al State qué puede esperar a recibir en cuando a tipos de valores! porque si no va a inferir que siempre vas a recibir el tipo de valor que usamos al setear el estado inicial
   const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL)
 
+  // DISPATCH ACTIONS
   // el handleRemove lo hago aquí porque aquí tengo el estado!
   const handleRemove = ({ id }: TodoId): void => {
-    const newTodos = todos.filter(todo => todo.id !== id)
-    setTodos(newTodos)
-  }
-
-  const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' | 'completed'>): void => {
-    const newTodos = todos.map(todo => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          // copia todo el resto del objeto y para el caso del completed -> lo invierte
-          // completed: !completed
-          completed
-        }
-      }
-      return todo
+    dispatch({
+      type: 'removeTodo',
+      payload: id
     })
-    setTodos(newTodos)
   }
 
+  const handleCompleted = ({ id }: Pick<TodoType, 'id'>): void => {
+    dispatch({
+      type: 'completeTodo',
+      payload: id
+    })
+  }
+
+  const handleRemoveCompleted = (): void => {
+    dispatch({
+      type: 'removeCompleted'
+    })
+  }
+
+  const handleAdd = ({ title }: TodoTitle): void => {
+    dispatch({
+      type: 'addTodo',
+      payload: title
+    })
+  }
+
+  // FILTERS
   const handleFilterChange = (filter: FilterValue): void => {
     setFilterSelected(filter)
   }
 
-  const handleRemoveCompleted = (): void => {
-    const newTodos = todos.filter(todo => !todo.completed)
-    setTodos(newTodos)
-  }
+  const activeCount = state.filter(todo => !todo.completed).length
+  const completedCount = state.length - activeCount
 
-  const handleAdd = ({ title }: TodoTitle): void => {
-    const newTodo = {
-      id: crypto.randomUUID(),
-      title,
-      completed: false
-    }
-    const addedTodos = [...todos, newTodo]
-    setTodos(addedTodos)
-  }
-
-  const activeCount = todos.filter(todo => !todo.completed).length
-  const completedCount = todos.length - activeCount
-
-  const filteredTodos = todos.filter(todo => {
+  const filteredTodos = state.filter(todo => {
     if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
     if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
     else return todo
